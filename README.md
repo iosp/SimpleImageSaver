d242e758278671d8b3d5bbe6d21704deb6331951
mhallak <mhallak@apollo>
2018-06-12 16:29:45 +0300

    Mainly the README

--- README.md
+++ README.md
@@ -3,3 +3,47 @@
 There is a new trend of developing algorithms with deep neural network (dnn).
 We are trying to test that idea on data sets produced by in a simulated world and environment.
 This python script is an example of how to produce thos kind of data sets.
+
+ATTENTION!!!!
+In order to be able to generate pictures, one need to activate the CAMERA(S) on the bobcat.
+To activate the cameras on the bobcat, remove comments on the model.sdf of the bobcat regarding the cameras
+in the live_bobcat directory:
+
+<!-- CAM Sensors  -->
+   <!-- <include>
+      <uri>model://FLEA3_L</uri>
+      <pose>-0.3 0.25 2.02 0 0 0</pose>
+      <name>left_flea3</name>
+    </include>
+    <joint name='left_flea3_joint' type='revolute'>
+      <child>left_flea3::camera_link</child>
+      <parent>body</parent>
+      <axis>
+        <xyz>0 1 0</xyz>
+        <limit>
+          <lower>0</lower>
+          <upper>0</upper>
+        </limit>
+        <dynamics/>
+      </axis>
+    </joint>
+    
+    <include>
+      <uri>model://FLEA3_R</uri>
+      <pose>-0.3 -0.25 2.02 0 0 0</pose>
+      <name>right_flea3</name>
+    </include>    
+    <joint name='right_flea3_joint' type='revolute'>
+      <child>right_flea3::camera_link</child>
+      <parent>body</parent>
+      <axis>
+        <xyz>0 1 0</xyz>
+        <limit>
+          <lower>0</lower>
+          <upper>0</upper>
+        </limit>
+        <xyz>0 1 0</xyz>
+      <dynamics/>
+      </axis>
+    </joint>
+-->
--- image_saver.py
+++ image_saver.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import sys
+print(sys.version_info)
 
 # rospy for the subscriber
 import rospy
@@ -22,32 +24,32 @@ def pose_callback(msg):
     global model_index , model_name 
 
     if not (model_name in msg.name) :
-	 	rospy.loginfo("Error : No vehicle named ' %s ' in the scen", model_name) 	 	
-    else:    	
-		model_index = msg.name.index(model_name)
-		myPose = msg.pose[model_index].position	
-		myOrientation = msg.pose[model_index].orientation 
+        rospy.loginfo("Error : No vehicle named ' %s ' in the scen", model_name) 	 	
+    else:
+        model_index = msg.name.index(model_name)
+        myPose = msg.pose[model_index].position	
+        myOrientation = msg.pose[model_index].orientation 
 
 def image_callback(msg):
     global myPose , myOrientation 
 
     file_name = "T=" + str(rospy.get_rostime()) + "   Px=" + str(round(myPose.x, 5)) + "   Py =" + str(round(myPose.y, 5)) + "   Pz =" + str(round(myPose.z, 5)) + "   Qx =" + str(round(myOrientation.x, 5)) + "   Qy =" + str(round(myOrientation.y, 5)) + "   Qz =" + str(round(myOrientation.z, 5)) + "   Qw=" + str(round(myOrientation.w, 5)) 
 
-    print (myPose) 
+    print (myPose)
     print (myOrientation)
     print ("Received an image!")
 
     try:
         # Convert your ROS Image message to OpenCV2
         cv2_img = bridge.imgmsg_to_cv2(msg, "bgr8")
-    except CvBridgeError, e:
+    except CvBridgeError as e:
         print(e)
-    else:  
-        # Save your OpenCV2 image as a png 
+    else:
+        # Save your OpenCV2 image as a png
         cv2.imwrite("/home/robil/ " + file_name + " .png", cv2_img)
 
 def main():
-    rospy.init_node('image_listener') 
+    rospy.init_node('image_listener')
     # Define your image topic
     image_topic = "/SENSORS/CAM/R"
     pose_topic = "/gazebo/model_states"
@@ -57,9 +59,9 @@ def main():
     rospy.Subscriber(image_topic, Image, image_callback)
     rospy.Subscriber(pose_topic, ModelStates, pose_callback)
 
-
     # Spin until ctrl + c
     rospy.spin()
 
 if __name__ == '__main__':
     main()
+    
